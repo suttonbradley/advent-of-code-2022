296287ab2d958d6b1e16d2eb6d26bd7fd549dc14
Sutton Bradley (HE/HIM) <suttonb@microsoft.com>
2022-12-07 09:50:46 -0800

    Add template dir

---
+++ template/src/lib.rs
@@ -0,0 +1,15 @@
+use std::{io::{Lines, BufReader}, fs::File};
+
+pub fn run(lines: Lines<BufReader<File>>) {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+
+    }
+}
